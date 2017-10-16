ea52afeacaff8724df0c972a18caf9c761cc57b6
Ubuntu <gim.kobayashi@gmail.com>
2017-10-16 11:48:50 +0000

    Y

--- src/App.kt
+++ src/App.kt
@@ -8,6 +8,7 @@ fun main(args : Array<String>) {
     kargs.contains("neologd") -> Neologd.main(args)
     kargs.contains("stat") -> Stat.main(args)
     kargs.contains("funktionale") -> Funktionale.main(args)
+    kargs.contains("yssadwords") -> YssAdwordsParser.main(args)
     else -> null
   }
 }
--- src/YssAdwordsParser.kt
+++ src/YssAdwordsParser.kt
@@ -13,86 +13,103 @@ import java.lang.Exception
 //@Serializable
 //data class Data(val key:String, val a: Int, @Optional val b: String = "42", val c: Long, val d: Boolean, val e : String?)
 
-fun main(args: Array<String>) = runBlocking<Unit> {
-  val fileNames = Files.newDirectoryStream(Paths.get("/home/ubuntu/xvdf/okeya/raw_data/listing"), "*").map { name ->
-    println(name)
-    name.toString()
-  }
-  fileScan@for ( fileName in fileNames )  { 
-    val lines = mutableListOf<String>() 
-    try { 
-      val inputStream = File(fileName).inputStream()
-      inputStream.bufferedReader().useLines { xs -> xs.forEach { lines.add(it)} }
-    } catch ( e : java.lang.Exception ) {
-      continue
+object YssAdwordsParser {
+  fun Adword( fileName:String, keys:List<String>, iter:Iterator<String>) {
+    println(fileName)
+    println(keys)
+    val df = mutableListOf<Map<String,String>>()
+    iter
+      .forEach { line ->
+        if ( Math.random() < 0.65 ) {
+          val vals = line.split(",")
+          val obj = keys.zip(vals).toMap()
+          //println(obj)  
+          df.add( obj )
+        }
     }
-    val iter = lines.iterator()
-    try {
-      val keys = when {
-        fileName.contains("ADWORDS") -> { iter.next(); iter.next().split(",") }
-        else -> continue@fileScan
+    df.map { 
+      val keyword = it["Keyword"]
+      val imps = it["Impressions"]?.toDouble() ?: 0.0
+      val campid = it["Campaign ID"]
+      Pair( keyword, imps )
+    }.groupBy {
+      it.first
+    }.toList().map {
+      val keyword = it.first
+      val arr = it.second
+      val sum = arr.map { it.second }.reduce{ y,x -> y+x }
+      Pair( keyword, sum )
+    }.let {
+      val text = it.toString()
+      val saveName = "/home/ubuntu/xvdf/okeya/raw_data/kotlinx-multithread-dataframe-design/outputs/" + fileName.split("/").last()
+      println( text )
+      PrintWriter(saveName).append(text).close()
+    }
+  }
+  
+  fun Yss( fileName:String, keys:List<String>, iter:Iterator<String>) {
+    println(fileName)
+    println(keys)
+    val df = mutableListOf<Map<String,String>>()
+    iter
+      .forEach { line ->
+        if ( Math.random() < 0.65 ) {
+          val vals = line.split(",")
+          val obj = keys.zip(vals).toMap()
+          //println(obj)  
+          df.add( obj )
+        }
+    }
+    df.map { 
+      val keyword = it["キーワード"]
+      val imps = it["インプレッション"]?.toDouble() ?: 0.0
+      //val campid = it["Campaign ID"]
+      Pair( keyword, imps )
+    }.groupBy {
+      it.first
+    }.toList().map {
+      val keyword = it.first
+      val arr = it.second
+      val sum = arr.map { it.second }.reduce{ y,x -> y+x }
+      Pair( keyword, sum )
+    }.let {
+      val text = it.toString()
+      val saveName = "/home/ubuntu/xvdf/okeya/raw_data/kotlinx-multithread-dataframe-design/outputs/" + fileName.split("/").last()
+      println( text )
+      PrintWriter(saveName).append(text).close()
+    }
+  }
+
+  fun main(args: Array<String>) = runBlocking<Unit> {
+    val fileNames = Files.newDirectoryStream(Paths.get("/home/ubuntu/xvdf/okeya/raw_data/listing"), "*").map { name ->
+      println(name)
+      name.toString()
+    }
+    fileScan@for ( fileName in fileNames )  { 
+      val lines = mutableListOf<String>() 
+      try { 
+        val inputStream = File(fileName).inputStream()
+        inputStream.bufferedReader().useLines { xs -> xs.forEach { lines.add(it)} }
+      } catch ( e : java.lang.Exception ) {
+        continue
       }
-      println(fileName)
-      println(keys)
-      val df = mutableListOf<Map<String,String>>()
-      iter
-        .forEach { line ->
-          if ( Math.random() < 0.1 ) {
-            val vals = line.split(",")
-            val obj = keys.zip(vals).toMap()
-            //println(obj)  
-            df.add( obj )
+      val iter:Iterator<String> = lines.iterator()
+      try {
+        when {
+          fileName.contains("ADWORDS") -> { 
+            iter.next() 
+            val keys = iter.next().split(",") 
+            Adword(fileName, keys, iter)
           }
+          fileName.contains("YSS_KEYWORDS_PERFORMANCE_REPORT") -> {
+            val keys = iter.next().split(",") 
+            Yss(fileName, keys, iter)
+          }
+          else -> continue@fileScan
+        }
+      } catch ( e : java.lang.Exception ) {
+        println(e)
       }
-      df.map { 
-        val keyword = it["Keyword"]
-        val imps = it["Impressions"]?.toDouble() ?: 0.0
-        val campid = it["Campaign ID"]
-        Pair( keyword, imps )
-      }.groupBy {
-        it.first
-      }.toList().map {
-        val keyword = it.first
-        val arr = it.second
-        val sum = arr.map { it.second }.reduce{ y,x -> y+x }
-        Pair( keyword, sum )
-      }.let {
-        val text = it.toString()
-        val saveName = "/home/ubuntu/xvdf/okeya/raw_data/kotlinx-multithread-dataframe-design/outputs/" + fileName.split("/").last()
-        println( text )
-        PrintWriter(saveName).append(text).close()
-      }
-    } catch ( e : java.lang.Exception ) {
-      println(e)
     }
-    
   }
-
-  val json = JSON.stringify(Data("Key", 42, "Default", 1000L, true, null)) 
-  println( json )
-
-  // フィールドが存在しないと、kotlinx.serialization.MissingFieldException を送出する
-  try {
-    val obj = JSON.parse<Data>("""{"a":42}""") // Data(a=42, b="42")
-  } catch( e : kotlinx.serialization.MissingFieldException  ) {
-    println( e )
-  }
-  // 問題なく復元できることの確認
-  val obj = JSON.parse<Data>(json)
-  println( obj )
-
-  (0..1000).map { key ->
-    val job = launch { // launch new coroutine
-      delay(1L) // non-blocking delay for 0.001 second (default time unit is ms)
-      val jedis = Jedis("localhost")
-      val keyStr = key.toString()
-      val json = JSON.stringify(Data(keyStr, 42, "Default", 1000L, true, null)) 
-      jedis.set(keyStr, json)
-      //println( jedis.get(keyStr) )
-    }
-    job
-  }.let { 
-    it.map { it.join() }
-  }
-
 }
